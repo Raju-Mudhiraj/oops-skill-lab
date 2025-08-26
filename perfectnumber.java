ec0b98703da5cef754c169fe2e5077b5708d5c23
Raju-Mudhiraj <nagarajumudhiraj34@gmail.com>
2025-08-26 12:07:58 +0530

    Add files via upload

---
+++ perfectnumber.java
@@ -0,0 +1,22 @@
+package cse;
+
+import java.util.Scanner;
+
+public class perfectnumber {
+ public static void main(String[] args){
+  int n,sum=0;
+  Scanner sc= new Scanner(System.in);
+  n=sc.nextInt();
+  for(int i=1;i<n;i++)
+  {
+   if(n%i==0)
+   {
+    sum=sum+i;
+   }
+  }
+  if(n==sum)
+  {
+   System.out.println("Perfect Number");
+  }
+ }
+}
